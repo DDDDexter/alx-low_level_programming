6a028771ab53cf3f633696168c94da60bcff6ff0
DDDDexter <anissarafa7@gmail.com>
2023-03-17 00:07:55 +0000

    K

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#INCLUDE <stdio.h>
+/**
+*main : this is a programme that prompt an string with the puts function
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);  
+}
