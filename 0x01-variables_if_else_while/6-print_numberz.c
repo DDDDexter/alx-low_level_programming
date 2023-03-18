8d23db57398cb9b35fb54fb34f165f1ca7aeb6b0
DDDDexter <anissarafa7@gmail.com>
2023-03-18 02:31:56 +0000

    L

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prompt sing numbers in base of 10.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
